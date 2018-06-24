8224ba227222b01df83d62f3fbe6ba06ff6a7d82
ArminasBek <arminasb7@gmail.com>
2018-06-24 12:13:20 +0100

    gulp watch initiated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch');
 
 gulp.task('default', function() {
   // place code for your default task here
   console.log("gulp runningas");
 });
+
+gulp.task('html', function() {
+  console.log("html task");
+})
+
+gulp.task('watch', function(){
+  watch('index.html', function(){
+    gulp.start('html');
+  })
+})
