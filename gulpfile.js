df1a536fbf9cf4da8a07f08eae6ccb2747b469b7
ArminasBek <arminasb7@gmail.com>
2018-06-24 12:59:27 +0100

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch    = require('gulp-watch'),
+sass     = require('gulp-sass');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -10,8 +11,24 @@ gulp.task('html', function() {
   console.log("html task");
 })
 
+gulp.task('css', function() {
+  console.log("css task");
+})
+
+gulp.task('sass', function(){
+  console.log("acctual sass task");
+  return gulp.src('./assets/css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+})
+
 gulp.task('watch', function(){
+  //watch index html file and triger html task
   watch('index.html', function(){
     gulp.start('html');
   })
-})
+  watch('assets/css/**/*.scss', function (){
+    gulp.start('sass');
+    console.log("sass-task completed");
+  });
+});
