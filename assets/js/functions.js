50189e983e999479d944e96fcaf5e58f5068704b
Amriesgrace <obieri.grace@gmail.com>
2018-07-27 02:21:32 +0100

    add location fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,54 +1,50 @@
-function getWeatherData(url, callback){
-    
-    var xmlhttp = new XMLHttpRequest(); 
-    xmlhttp.onreadystatechange = function(){
-        if(xmlhttp.readyState ==  4 && xmlhttp.status == 200){
-            let data = JSON.parse(xmlhttp.responseText);
-            //console.log(data);
-            callback(data);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
-//get user location if allowed 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
         let long  = position.coords.longitude;
         let lat = position.coords.latitude;
-        getWeatherData('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=2c044a28375a1ac42ebebf34784efa17', function(data){
-            
 
-            let ktemp = data.main.temp;
-            let cityName = data.name;
-            //console.log(cityName);
-            let desc = data.weather[0].description;
-            let icon = data.weather[0].icon;
-            //console.log(icon);
-            var iconURL = "http://openweathermap.org/img/w/" +icon+".png";
-            //console.log(iconURL);
-            
-            function kToC (){
-                cTemp = Math.round(ktemp - 273);
-            }
-            kToC(); //convert temp to celsius
-        
-            //for screen display
-            let tempDisplay = document.getElementById("temp");
-            let weatherDesc = document.getElementById("weather_description");
-            let cityDisplay = document.getElementById("city");
-            let weatherIcon = document.getElementById("icon");
-            let weather = document.getElementById("weather");
+        console.log(lat);
+        console.log(long);
+        let api = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=2c044a28375a1ac42ebebf34784efa17';
+            fetch(api)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data) // Prints result from `response.json()` in getRequest
+                    //console.log(data.name)
+                    let cityName = data.name;
+                    let desc = data.weather[0].description;
+                    let ktemp = data.main.temp;
+                    let icon = data.weather[0].icon;
+                    let iconURL = "http://openweathermap.org/img/w/" +icon+".png";
+                    let tempDisplay = document.getElementById("temp");
+                    let cityDisplay = document.getElementById("city");
+                    let weatherIcon = document.getElementById("icon");
+                    let weatherDescription = document.getElementById("desc");
+                    let weather = document.getElementsByClassName("weather");
 
-            tempDisplay.innerHTML = cTemp;
-            cityDisplay.innerHTML = cityName;
 
-            weatherDesc.innerHTML = desc;
+                    //console.log(icon);
+                    //console.log(iconURL);
+                    //console.log(api)
 
-            weatherIcon.src = iconURL;
+                    function kToC (){
+                        cTemp = Math.round(ktemp - 273);
+                    }
+                    kToC();//converts temp to celsius
+                
 
-            weather.title = desc;
-        })
+                    //for screen display
+                    tempDisplay.innerHTML = cTemp;
+                    cityDisplay.innerHTML = cityName;
+
+                    weatherIcon.src = iconURL;
+                    weatherDescription.innerHTML = desc;
+
+                    weather[0].title = desc;
+                        cityDisplay.innerHTML = cityName;
+                    })
+            .catch(error => console.error(error))      
     })
 } 
+
 
